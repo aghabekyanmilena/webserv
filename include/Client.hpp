e1ea16cc25ecec2d8b017e783585ad36db3a5904
Anush Tserunyan <atseruny@c2r1s4.42yerevan.am>
2026-09-23 20:12:49 +0400

    half of Clent class' implementation

--- include/Client.hpp
+++ include/Client.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <ctime>
+#include <iostream>
 
 class Client
 {
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,2 +1,33 @@
 #include "Client.hpp"
 
+Client::Client(int fd) : _fd(fd) { }
+
+Client::~Client() { }
+
+int Client::getFd() const
+{
+	return _fd;
+}
+
+void Client::updateActivity()
+{
+	_lastActivity = std::time(NULL);
+}
+
+std::time_t Client::getLastActivity() const
+{
+	return _lastActivity;
+}
+
+bool Client::hasTimedOut(std::time_t now, int timeoutSeconds) const
+{
+	int elapsed_seconds = std::difftime(now, _lastActivity);
+
+	if (elapsed_seconds >= timeoutSeconds)
+	{
+        std::cout << "Timeout reached! Elapsed: " << elapsed_seconds << "s\n";
+		return true;
+    }
+	std::cout << "Completed within time. Elapsed: " << elapsed_seconds << "s\n";
+    return false;
+}
